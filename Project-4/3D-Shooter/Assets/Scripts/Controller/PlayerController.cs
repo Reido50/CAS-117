41bf5db2e0204fca2fde93e01f684b47fc7626aa
Reid Harry <harryrei@msu.edu>
2020-11-18 20:27:45 -0600

    Finished Part 16

--- Project-4/3D-Shooter/Assets/Scripts/Controller/PlayerController.cs
+++ Project-4/3D-Shooter/Assets/Scripts/Controller/PlayerController.cs
@@ -7,7 +7,33 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
+    [Header("Settings")]
+    [Tooltip("The speed at which the player moves")]
+    public float moveSpeed = 2f;
+    [Tooltip("The speed at which the player rotates to look left or right (calculated in degrees")]
+    public float lookSpeed = 60f;
+    [Tooltip("The power at which the player jumps")]
+    public float jumpPower = 8f;
+    [Tooltip("The strength of gravity")]
+    public float gravity = 9.81f;
+
+    [Header("Jump Timing")]
+    public float jumpTimeLeniency = 0.1f;
+    float timeToStopBeingLenient = 0.0f;
+
+    [Header("Required References")]
+    [Tooltip("The player shooter script that fires projectiles")]
     public Shooter playerShooter;
+    [Tooltip("The player health script that tracks health and lives")]
+    public Health playerHealth;
+    public List<GameObject> disableWhileDead;
+    [Tooltip("True if the double jump is available to use")]
+    bool doubleJumpAvailable = false;
+
+    /// The character controller component on the player
+    private CharacterController controller;
+    /// The input manager component on the player
+    private InputManager inputManager;
 
     /// <summary>
     /// Description:
@@ -19,7 +45,22 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Start()
     {
+        SetUpInputManager();
+        SetUpCharacterController();
+    }
 
+    private void SetUpCharacterController()
+    {
+        controller = GetComponent<CharacterController>();
+        if(controller == null)
+        {
+            Debug.LogError("No CharacterController attached the gameobject of the PlayerController");
+        }
+    }
+
+    void SetUpInputManager()
+    {
+        inputManager = InputManager.instance;
     }
 
     /// <summary>
@@ -32,6 +73,84 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
+        if(playerHealth.currentHealth <= 0)
+        {
+            foreach(GameObject inGameObject in disableWhileDead)
+            {
+                inGameObject.SetActive(false);
+            }
+            return;
+        }
+        else
+        {
+            foreach (GameObject inGameObject in disableWhileDead)
+            {
+                inGameObject.SetActive(true);
+            }
+        }
 
+        ProcessMovement();
+        ProcessRotation();
+    }
+
+    Vector3 moveDirection;
+
+    void ProcessMovement()
+    {
+        // Get input from input manager
+        float leftRightInput = inputManager.horizontalMoveAxis;
+        float forwardBackwardInput = inputManager.verticalMoveAxis;
+        bool jumpPressed = inputManager.jumpPressed;
+
+        // Handle the control of the player while it is on the ground
+        if (controller.isGrounded)
+        {
+            doubleJumpAvailable = true;
+            timeToStopBeingLenient = Time.time + jumpTimeLeniency;
+
+            // Set the movement direction to be the recieved input, set y to 0 since we are on the ground
+            moveDirection = new Vector3(leftRightInput, 0, forwardBackwardInput);
+            moveDirection *= moveSpeed;
+
+            if (jumpPressed)
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(leftRightInput * moveSpeed, moveDirection.y, forwardBackwardInput * moveSpeed);
+
+            if(jumpPressed && Time.time < timeToStopBeingLenient)
+            {
+                moveDirection.y = jumpPower;
+            }
+            else if(jumpPressed && doubleJumpAvailable)
+            {
+                moveDirection.y = jumpPower;
+                doubleJumpAvailable = false;
+            }
+        }
+
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            moveDirection.y = -0.3f;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    void ProcessRotation()
+    {
+        float horizonalLookInput = inputManager.horizontalLookAxis;
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(playerRotation.x,
+            playerRotation.y + horizonalLookInput * lookSpeed * Time.deltaTime,
+            playerRotation.z));
     }
 }
